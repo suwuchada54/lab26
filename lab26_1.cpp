c9f73e4bb1cc688ad930d686c259ece414ec8dbd
suwichada <jippee.1211@gmail.com>
2022-02-18 15:50:09 +0700

    lab26_1.cpp

--- lab26_1.cpp
+++ lab26_1.cpp
@@ -63,4 +63,51 @@ void List::append(int d){
 	size++;
 }
 
-//Write List::remove() here
+void List::remove(int idx){
+	Node *n = new Node;
+
+	if(idx == 0){
+		n = root;
+		root = root->next;
+		delete n;
+		size --;
+	}
+	else{
+	Node *current = root;
+	for(int i=0; i<idx-1; i++){
+		current = current->next;
+	}
+	n = current->next;
+	current->next = current->next->next;
+	delete n;
+	size--;
+	}
+}
+
+int main(){
+	List myList = {0,0};
+	myList.append(5);
+	myList.append(7);
+	myList.append(11);
+	myList.append(4);
+	myList.append(12);
+	myList.append(45);
+
+	myList.insert(29,2);
+	myList.insert(33,3);
+	myList.insert(77,5);
+	myList.insert(69,0);
+	myList.show();
+	cout << "\n"; 
+
+	myList.remove(2);
+	myList.show();
+	cout << "\n"; 
+	myList.remove(4);
+	myList.show();
+	cout << "\n";
+	myList.remove(0);
+	myList.show();
+	cout << "\n";
+	return 0 ;
+}
